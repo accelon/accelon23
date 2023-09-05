f4b3ce525f6fcbc990cdcb699efd9907f2f4eec6
yap cheah shen <yapcheahshen@gmail.com>
2023-09-05 19:06:31 +0800

    reopen udpated ptk

--- src/checkupdate.svelte
+++ src/checkupdate.svelte
@@ -2,6 +2,7 @@
 import {ptks} from './store.js'
 import {onMount} from 'svelte'
 import {isLatest,downloadToCache} from 'ptk/platform/downloader.js'
+import {poolDel,openPtk} from 'ptk'
 import {_} from './textout.ts'
 import { CacheName } from './constant.js';
 $: updatestatus=ptks.map(it=>[it, 'checking']);
@@ -15,22 +16,30 @@ onMount(async ()=>{
     updatestatus=updatestatus;
 })
 
-let downloading=false;
+let downloading=false,downloadmsg='';
 const updateptk=async idx=>{
     const name=updatestatus[idx][0];
     if (downloading)return;
     downloading=true;
     //append timestamp to url to force check 
-    const res=await downloadToCache(CacheName,name+'.ptk?'+(new Date()).toISOString()); 
+    const res=await downloadToCache(CacheName,name+'.ptk?'+(new Date()).toISOString(),msg=>{
+        downloadmsg=msg;
+    });
+
+    poolDel(name);
+    const buf=await res.arrayBuffer();
+    await openPtk(name,new Uint8Array(buf));
     downloading=false;
     updatestatus[idx][1]='';
     updatestatus=updatestatus;
+    downloadmsg='';
+    needupdate--;
 }
 </script>
 {#each updatestatus as [ptkname,status],idx}
 {#if status=='hasupdate'} 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-<span class="clickable hyperlink" on:click={()=>updateptk(idx)}>{ptkname+'更新'+(downloading?'中':'')}</span>{/if}
+<span class="clickable hyperlink" on:click={()=>updateptk(idx)}>{ptkname+'更新'+downloadmsg}</span>{/if}
 {/each}
-{#if !needupdate}{_("所有數據皆是最新版。")}{/if}
+{#if needupdate<1}{_("所有數據皆是最新版。")}{/if}
